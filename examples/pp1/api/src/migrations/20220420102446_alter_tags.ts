fda96eb7376f8f1fe532ea1835e0b31b092c5c9d
Minsang Kim <minsangk@me.com>
2025-10-31 16:44:23 +0900

    [workspace] pp1 example 삭제 (미사용)

--- examples/pp1/api/src/migrations/20220420102446_alter_tags.ts
+++
@@ -1,9 +0,0 @@
-import { Knex } from "knex";
-
-export async function up(_knex: Knex): Promise<void> {
-  // return knex.schema.alterTable("tags", (table) => {});
-}
-
-export async function down(_knex: Knex): Promise<void> {
-  // return knex.schema.alterTable("tags", (table) => {});
-}
--- examples/pp1/api/src/testing/global.ts
+++
@@ -1,8 +0,0 @@
-import { FixtureManager } from "sonamu";
-require("dotenv").config();
-
-export async function setup() {
-  return async function teardown() {
-    await FixtureManager.destory();
-  };
-}
