250527a7c8f03aa25f0cec41a5d389af7e82bfce
potados99 <potados99@gmail.com>
2025-11-10 13:35:46 +0900

    [sonamu] fix: 엔티티 bool 타입 기본값을 대문자 "FALSE"로 주면 "false"가 아니라서 replaceColumnDefaultTo를 타지 못해 defaultTo가 "0"으로 치환되지 못함에 따라 마이그레이터가 자꾸 DB의 기본값인 0을 "FALSE"로 바꾸는 마이그레이션을 생성하려 하는 문제 해결
    defaultTo의 내용을 비교할 때, toLowerCase()로 낮춰서 비교하도록 했어요. col.defaultTo가 어느 정도로 전처리(trim 등)된 친구인지 모르겠으나 이 외의 처리는 안 하는 분위기인 것 같아서 그냥 col.defaultTo.toLowerCase()만 했습니다.

--- modules/sonamu/src/migration/code-generation.ts
+++ modules/sonamu/src/migration/code-generation.ts
@@ -680,9 +680,9 @@ export async function generateAlterCode(
     // boolean인 경우 기본값 정규화 (MySQL에서는 TINYINT(1)로 저장되므로 0 또는 1로 정규화)
     // TODO: db.ts에 typeCase 설정 확인하여 처리하도록 수정 필요
     if (col.type === "boolean" && col.defaultTo !== undefined) {
-      if (col.defaultTo === "0" || col.defaultTo === "false") {
+      if (col.defaultTo === "0" || col.defaultTo.toLowerCase() === "false") {
         col.defaultTo = "0";
-      } else if (col.defaultTo === "1" || col.defaultTo === "true") {
+      } else if (col.defaultTo === "1" || col.defaultTo.toLowerCase() === "true") {
         col.defaultTo = "1";
       }
     }
