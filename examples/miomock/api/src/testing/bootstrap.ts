084b9e2c00bceb2e09132119882134b66ed8b4a5
nakkim <nakkim.dev@gmail.com>
2025-11-10 20:00:08 +0900

    [miomock] fixture cleanAndSeed 테이블명 추가

--- examples/miomock/api/src/testing/bootstrap.ts
+++ examples/miomock/api/src/testing/bootstrap.ts
@@ -1,13 +1,13 @@
-import { FixtureManager, Sonamu } from "sonamu";
+import { DatabaseSchemaExtend, FixtureManager, Sonamu } from "sonamu";
 import { afterAll, afterEach, beforeAll, beforeEach, vi } from "vitest";
 
-export function bootstrap() {
+export function bootstrap(tableNames?: (keyof DatabaseSchemaExtend)[]) {
   beforeAll(async () => {
     await Sonamu.initForTesting();
     FixtureManager.init();
   });
   beforeEach(async () => {
-    await FixtureManager.cleanAndSeed();
+    await FixtureManager.cleanAndSeed(tableNames);
     vi.clearAllMocks();
   });
   afterEach(() => {
