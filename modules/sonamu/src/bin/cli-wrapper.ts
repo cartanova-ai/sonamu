a584feec99fa42afb81faba496ea9ebc19c1ccf6
potados99 <potados99@gmail.com>
2025-11-12 14:13:56 +0900

    [sonamu] refactor: cli-wrapper 약간의 리팩토링

--- modules/sonamu/src/bin/cli-wrapper.ts
+++ modules/sonamu/src/bin/cli-wrapper.ts
@@ -14,8 +14,9 @@ const scriptPath = resolve(__dirname, "cli.js");
 const args = process.argv.slice(2);
 
 // build 명령어는 dist 없이도 실행 가능하도록 cli.ts 외부에서 처리(Sonamu.init에서 dist 필요)
-function build(checkTypes: boolean = false) {
+function build() {
   try {
+    console.log(chalk.blue("Removing build directory..."));
     if (existsSync(BUILD_DIR)) {
       rmSync(BUILD_DIR, { recursive: true, force: true });
     }
@@ -25,29 +26,31 @@ function build(checkTypes: boolean = false) {
   }
 
   try {
+    console.log(chalk.blue("Building with swc..."));
     execSync(SWC_BUILD_COMMAND, { cwd: process.cwd(), stdio: "inherit" });
   } catch (error) {
     console.error(chalk.red("Build failed."), error);
     process.exit(1);
   }
+}
 
-  if (checkTypes) {
-    try {
-      console.log(chalk.blue("Checking types with tsc..."));
-      execSync(TSC_TYPE_CHECK_COMMAND, {
-        cwd: process.cwd(),
-        stdio: "inherit",
-      });
-    } catch (error) {
-      console.error(chalk.red("Type check failed."), error);
-      process.exit(1);
-    }
+function checkTypes() {
+  try {
+    console.log(chalk.blue("Checking types with tsc..."));
+    execSync(TSC_TYPE_CHECK_COMMAND, {
+      cwd: process.cwd(),
+      stdio: "inherit",
+    });
+  } catch (error) {
+    console.error(chalk.red("Type check failed."), error);
+    process.exit(1);
   }
 }
 
 if (args[0] === "build") {
   console.log(chalk.blue("Building the project..."));
-  build(true);
+  build();
+  checkTypes();
   console.log(chalk.green("Build completed successfully."));
   process.exit(0);
 }
