b16a70f33c9ad58fc4f14ffc143b54d42678c976
nakkim <nakkim.dev@gmail.com>
2025-11-12 16:31:26 +0900

    [miomock] project.image_urls 컬럼 추가

---
+++ examples/miomock/api/src/migrations/20251112135740_alter_projects_add1.ts
@@ -0,0 +1,15 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable("projects", (table) => {
+    // add
+    table.json("image_urls").nullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.alterTable("projects", (table) => {
+    // rollback - add
+    table.dropColumns("image_urls");
+  });
+}
