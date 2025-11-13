acb5e28531f48bf2742f879c4dc060287b27670d
nakkim <nakkim.dev@gmail.com>
2025-11-13 18:49:21 +0900

    [miomock/api] sonamu.config.ts 적용

---
+++ examples/miomock/api/sonamu.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from "sonamu";
+
+export default defineConfig({
+  projectName: "Miomock",
+  api: {
+    dir: "api",
+    timezone: "Asia/Seoul",
+    route: {
+      prefix: "/api",
+    },
+  },
+  sync: {
+    targets: ["web"],
+  },
+  ui: {
+    port: 60000,
+  },
+  database: {
+    name: "miomock",
+    defaultOptions: {
+      connection: {
+        host: process.env.MIOMOCK_DB_HOST ?? "0.0.0.0",
+        port: Number(process.env.MIOMOCK_DB_PORT ?? 3306),
+        user: process.env.MIOMOCK_DB_USER ?? "root",
+        password: process.env.MIOMOCK_DB_PASSWORD ?? "miomock123",
+        typeCast: function (field: any, next: any) {
+          if (field.type == "TINY" && field.length == 1) {
+            const value = field.string();
+            return value ? value == "1" : null;
+          }
+          // DATE 타입은 문자열로 유지 (YYYY-MM-dd 형태)
+          if (field.type == "DATE") {
+            return field.string();
+          }
+          return next();
+        },
+      },
+    },
+  },
+});
