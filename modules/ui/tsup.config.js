9b393ad3572c6b7b39f34444184352bf13699c44
potados99 <potados99@gmail.com>
2025-11-14 18:43:42 +0900

    [ui] fix: 모듈은 esm으로, 타겟은 esnext로 맞춤

--- modules/ui/tsup.config.js
+++ modules/ui/tsup.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["node/index.ts"],
   dts: true,
-  format: ["cjs"],
-  target: "es2020",
+  format: ["esm"],
+  target: "esnext",
   clean: true,
   sourcemap: true,
   shims: true,
