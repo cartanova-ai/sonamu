cd20074af420a444ccd6851203eb0439ef1eef4f
Minsang Kim <minsangk@me.com>
2025-11-16 11:43:04 +0900

    [miomock] 테스트트랜잭션격리 테스트 코드를 Puri를 사용하게 변경

--- examples/miomock/api/src/application/user/user.model.test.ts
+++ examples/miomock/api/src/application/user/user.model.test.ts
@@ -1,44 +1,55 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, assert } from "vitest";
 import { bootstrap } from "../../testing/bootstrap";
 import { UserModel } from "./user.model";
 import { range } from "lodash";
-import { DB } from "sonamu";
+import { BaseModel } from "sonamu";
 
 bootstrap();
 describe("UserModel", () => {
-  const func1 = async () => {
-    const user = await UserModel.findById("A", 1);
-    expect(user.username).toBe("Minsang Kim");
-    expect(true).toBe(true);
+  const func1 = (no: number) => {
+    return async () => {
+      const user = await UserModel.findById("A", 1);
+      expect(user.username).toBe("Minsang Kim");
+      expect(true).toBe(true);
 
-    const db = DB.getDB("w");
-    await db.transaction(async (trx) => {
-      await trx.table("users").insert({
-        username: "testuser",
-        email: "testuser@test.com",
-        password: "testpassword",
-        role: "normal",
-      });
-      await trx.table("users").where("id", 1).delete();
-      await trx.table("companies").insert({
-        name: "testcompany",
-      });
-      const company = await trx("companies").select("id").first();
-      await trx.table("departments").insert({
-        name: "testdepartment",
-        company_id: company.id,
-      });
-      await trx.table("tags").insert({
-        name: "testtag",
-      });
-    });
+      const db = BaseModel.getPuri("w");
+      await db.transaction(async (trx) => {
+        // @ts-ignore: unused
+        const res1 = await trx
+          .table("users")
+          .insert({
+            username: `testuser${no}`,
+            email: `testuser${no}@test.com`,
+            password: "testpassword",
+            role: "normal",
+          })
+          .debug();
+        // @ts-ignore: unused
+        const resDelete = await trx.table("users").where("id", 1).delete();
 
-    const usernames = await db.table("users").pluck("username");
-    expect(usernames).toHaveLength(1);
-    // console.log(usernames);
+        await trx.table("companies").insert({
+          name: "testcompany",
+        });
+        const company = await trx
+          .table("companies")
+          .select({ id: "id" })
+          .first();
+        assert(company);
+        await trx.table("departments").insert({
+          name: `testdepartment${no}`,
+          company_id: company.id,
+        });
+        await trx.table("tags").insert({
+          name: `testtag${no}`,
+        });
+      });
+
+      const usernames = await db.table("users").pluck("username");
+      expect(usernames).toHaveLength(1);
+    };
   };
-  range(0, 200).map(async (i) => {
-    test(`Testing#${i + 1}`, func1);
+  range(0, 50).map(async (i) => {
+    test(`Testing#${i + 1}`, func1(i));
   });
 
   test("should get my IP", async () => {
--- examples/miomock/api/src/testing/fixture.test.ts
+++ examples/miomock/api/src/testing/fixture.test.ts
@@ -2,13 +2,15 @@ import { bootstrap } from "../testing/bootstrap";
 import { describe, expect, test } from "vitest";
 import { loadFixtures } from "./fixture";
 
-bootstrap(["companies"]);
+bootstrap();
 describe("픽스쳐", () => {
   test("전체 픽스쳐 테스트", async () => {
     const f = await loadFixtures(["company01"]);
 
     expect(f.company01.id).toBe(1);
-    expect(f.company01.created_at).toBe("2025-10-24T05:50:32.000Z");
+    expect(f.company01.created_at.getTime()).toBe(
+      new Date("2025-10-24T05:50:32.000Z").getTime()
+    );
     expect(f.company01.name).toBe("테크놀로지 주식회사");
   });
 });
