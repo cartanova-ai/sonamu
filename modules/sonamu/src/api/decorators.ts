ccf7555fcb419fe52e2817594d0a2d2aaeba6ec5
nakkim <nakkim.dev@gmail.com>
2025-11-18 17:38:51 +0900

    [sonamu] req.files 접근시 hang 되는 이슈 수정

--- modules/sonamu/src/api/decorators.ts
+++ modules/sonamu/src/api/decorators.ts
@@ -225,6 +225,7 @@ export function upload(options: UploadDecoratorOptions = {}) {
         const rawFilesIterator = request.files();
         for await (const rawFile of rawFilesIterator) {
           if (rawFile) {
+            await rawFile.toBuffer();
             uploadContext.files.push(new FileStorage(rawFile, storage));
           }
         }
