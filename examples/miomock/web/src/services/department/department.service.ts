d1fab1f73fc002bff780e165dec4c3057910d6e0
potados99 <potados99@gmail.com>
2025-11-19 18:22:57 +0900

    [miomock] chore: 서비스 파일 업데이트

--- examples/miomock/web/src/services/department/department.service.ts
+++ examples/miomock/web/src/services/department/department.service.ts
@@ -12,6 +12,7 @@ import {
   SSEStreamOptions,
   useSSEStream,
 } from "../sonamu.shared";
+import { AxiosProgressEvent } from "axios";
 import {
   DepartmentSubsetKey,
   DepartmentSubsetMapping,
