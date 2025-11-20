b63893eca6eb3bbd9812074a9e5d3896945e88c7
Minsang Kim <minsangk@me.com>
2025-11-20 18:01:29 +0900

    [workspace] vite, vitest 버전 상향

--- examples/miomock/api/vite.config.ts
+++ examples/miomock/api/vite.config.ts
@@ -3,16 +3,15 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [],
   test: {
+    include: ["src/**/*.test.ts"],
     globals: true,
     globalSetup: ["./src/testing/global.ts"],
     pool: "forks",
-    poolOptions: {
-      forks: {
-        minForks: 1,
-        maxForks: 1,
-        isolate: false,
-      },
-    },
+    maxWorkers: 1,
+    isolate: false,
     environment: "node",
+  },
+  optimizeDeps: {
+    include: ["lodash-es", "axios", "uuid"],
   },
 });
