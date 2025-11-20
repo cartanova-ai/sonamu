a69bc58d6943eeb6f14b5ea15044763e074596b2
potados99 <potados99@gmail.com>
2025-11-20 14:33:12 +0900

    [miomock] fix: lodash-es로 전환

--- examples/miomock/api/src/testing/bootstrap.ts
+++ examples/miomock/api/src/testing/bootstrap.ts
@@ -1,4 +1,4 @@
-import { cloneDeep } from "lodash";
+import { cloneDeep } from "lodash-es";
 import { Context, DB, FixtureManager, Sonamu } from "sonamu";
 import { afterAll, afterEach, beforeAll, beforeEach, vi } from "vitest";
 
