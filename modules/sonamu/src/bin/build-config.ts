13d6825c39ded2f2a1e3830f85467b44f79505f9
potados99 <potados99@gmail.com>
2025-11-23 18:28:41 +0900

    [sonamu] feat: pnpm build할 때 사용자 프로젝트의 root에 .swcrc가 없다면 sonamu를 빌드할 때 쓰는 그 .swcrc를 가져다 쓰게 해요

--- modules/sonamu/src/bin/build-config.ts
+++ modules/sonamu/src/bin/build-config.ts
@@ -1,4 +1,9 @@
 /**
+ * 소스 코드가 담길 디렉토리 경로
+ */
+export const SRC_DIR = "src";
+
+/**
  * 빌드 결과물이 담길 디렉토리 경로
  */
 export const BUILD_DIR = "dist";
@@ -7,8 +12,9 @@ export const BUILD_DIR = "dist";
  * SWC 빌드 명령어
  * .swcrc 설정 사용
  */
-export const SWC_BUILD_COMMAND = `swc src -d ${BUILD_DIR} --strip-leading-paths`;
-
+export const SWC_BUILD_COMMAND = (configFilePath: string) =>
+    `swc ${SRC_DIR} -d ${BUILD_DIR} --config-file ${configFilePath}  --strip-leading-paths`;
+  
 /**
  * TSC 타입 체크 명령어
  */
