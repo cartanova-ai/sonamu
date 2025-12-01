845a1cc93f36e3df5b3909da55c4f8ff75577eae
Minsang Kim <minsangk@me.com>
2025-12-01 16:27:42 +0900

    [sonamu] chore: t1 테스트 삭제

--- examples/miomock/api/src/practices/t1.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { t1 } from "./t1";
-
-describe("t1", () => {
-  it("should return t1", () => {
-    expect(t1()).toBe("t1");
-  });
-});
--- examples/miomock/api/src/practices/t1.ts
+++
@@ -1,3 +0,0 @@
-export function t1() {
-  return "t1";
-}
