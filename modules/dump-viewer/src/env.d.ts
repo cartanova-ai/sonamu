8136663808c2263262ae3164e11c269c83399dcf
potados99 <potados99@gmail.com>
2025-12-02 20:55:05 +0900

    [dump-viewer] chore: 안 쓰는 dump-viewer 삭제

--- modules/dump-viewer/src/env.d.ts
+++
@@ -1,5 +0,0 @@
-type HotHookDump = typeof import("./fixtures/dump.json");
-
-interface Window {
-  __HOT_HOOK_DUMP__: HokHookDump;
-}
--- modules/dump-viewer/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import '@unocss/reset/tailwind-compat.css'
-import 'virtual:uno.css'
-import './index.css'
-
-import { render } from 'preact'
-
-import { App } from './app.tsx'
-
-render(<App />, document.getElementById('app')!)
