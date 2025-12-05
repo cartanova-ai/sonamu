e8117625cff47ec4c687c620fbb72e20185169b7
Minsang Kim <minsangk@me.com>
2025-12-05 18:26:03 +0900

    [miomock/api] 기존 entity.json 스키마에서 EnumProp에 length 필드 전부 삭제

--- modules/sonamu/src/entity/entity-manager.ts
+++ modules/sonamu/src/entity/entity-manager.ts
@@ -6,7 +6,6 @@ import path from "path";
 import { prettifyError } from "zod";
 import { Sonamu } from "../api/sonamu";
 import { type EntityJson, EntityJsonSchema } from "../types/types";
-import { isTest } from "../utils/controller";
 import type { AbsolutePath } from "../utils/path-utils";
 import { Entity } from "./entity";
 
@@ -39,10 +38,9 @@ class EntityManagerClass {
       if (error) {
         const relativePath = path.relative(Sonamu.apiRootPath, file);
         const errorMessage = prettifyError(error);
-        !isTest() &&
-          console.error(
-            chalk.red(`Invalid entity.json schema: ${relativePath}\n${chalk.yellow(errorMessage)}`),
-          );
+        console.error(
+          chalk.red(`Invalid entity.json schema: ${relativePath}\n${chalk.yellow(errorMessage)}`),
+        );
       }
 
       await this.register(json);
