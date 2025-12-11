10b92fb197114f2825b9433f3c55149dfe987e91
DongSeonYoo <inko51366@naver.com>
2025-12-11 16:09:23 +0900

    [sonamu/create-sonamu]: 템플릿 동기화

--- modules/create-sonamu/template/src/api/database/scripts/seed.sh
+++ modules/create-sonamu/template/src/api/database/scripts/seed.sh
@@ -35,6 +35,10 @@ psql \
   -p "${DB_PORT}" \
   -U "${DB_USER}" \
   -d postgres \
+  -c "SELECT pg_terminate_backend(pg_stat_activity.pid)
+  FROM pg_stat_activity
+  WHERE datname = '${FIXTURE_DB}'
+    AND pid <> pg_backend_pid();" \
   -c "DROP DATABASE IF EXISTS \"${FIXTURE_DB}\";" \
   -c "CREATE DATABASE \"${FIXTURE_DB}\";"
 
