84d6aefb7996d3cdcd34f71cc920ade8684643a7
potados99 <potados99@gmail.com>
2025-12-15 15:30:12 +0900

    [sonamu] fix: 터미널이 글자보다 작을 때 centerText를 써서 출력하려고 하면 margin 계산이 음수(!)가 되어 터지는 문제 해결

--- modules/sonamu/src/utils/console-util.ts
+++ modules/sonamu/src/utils/console-util.ts
@@ -1,4 +1,4 @@
 export function centerText(text: string): string {
-  const margin = (process.stdout.columns - text.length) / 2;
+  const margin = Math.max(0, Math.floor((process.stdout.columns - text.length) / 2));
   return " ".repeat(margin) + text + " ".repeat(margin);
 }
