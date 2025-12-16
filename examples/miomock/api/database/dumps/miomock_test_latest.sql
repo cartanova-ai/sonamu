8912dbe5c611d6c42198b51695992653e4455c77
DongSeonYoo <inko51366@naver.com>
2025-12-17 04:04:55 +0900

    [sonamu]: 잘못된 마이그레이션 파일 삭제

--- examples/miomock/api/database/dumps/miomock_test_latest.sql
+++ examples/miomock/api/database/dumps/miomock_test_latest.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict 62nppBXPfhqCg20YV8ORSzpsbCy8ufRyAJw2v1HCh11TDvclTgHVlLXc4s59nXF
+\restrict jLO9hoWtobAfryKDR8HvkssZdzNkbUj5PT1DR3vwJrzv9JsicErwTSYSD3mIgNO
 
 -- Dumped from database version 18.1 (Debian 18.1-1.pgdg12+2)
 -- Dumped by pg_dump version 18.1
@@ -641,7 +641,6 @@ INSERT INTO public.knex_migrations VALUES (53, '20251215123723_alter_projects_ad
 INSERT INTO public.knex_migrations VALUES (54, '20251215123829_alter_projects.ts', 5, '2025-12-15 12:38:33.09+09');
 INSERT INTO public.knex_migrations VALUES (55, '20251216132209_alter_projects_drop1.ts', 6, '2025-12-16 13:38:53.379+09');
 INSERT INTO public.knex_migrations VALUES (56, '20251216132334_alter_projects_add1.ts', 6, '2025-12-16 13:38:53.383+09');
-INSERT INTO public.knex_migrations VALUES (57, '20251217031141_alter_documents_add1_drop2.ts', 7, '2025-12-17 03:39:51.931+09');
 
 
 --
@@ -1754,14 +1753,14 @@ INSERT INTO public.users VALUES (12, '2023-11-01 01:00:00+09', 'deleted@test.com
 -- Name: companies_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.companies_id_seq', 299, true);
+SELECT pg_catalog.setval('public.companies_id_seq', 308, true);
 
 
 --
 -- Name: departments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.departments_id_seq', 55, true);
+SELECT pg_catalog.setval('public.departments_id_seq', 78, true);
 
 
 --
@@ -1775,7 +1774,7 @@ SELECT pg_catalog.setval('public.documents_id_seq', 1, false);
 -- Name: employees_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.employees_id_seq', 640, true);
+SELECT pg_catalog.setval('public.employees_id_seq', 663, true);
 
 
 --
@@ -1838,7 +1837,7 @@ SELECT pg_catalog.setval('public.tags_id_seq', 8, true);
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 2291, true);
+SELECT pg_catalog.setval('public.users_id_seq', 2361, true);
 
 
 --
@@ -2060,5 +2059,5 @@ ALTER TABLE ONLY public.projects__employees
 -- PostgreSQL database dump complete
 --
 
-\unrestrict 62nppBXPfhqCg20YV8ORSzpsbCy8ufRyAJw2v1HCh11TDvclTgHVlLXc4s59nXF
+\unrestrict jLO9hoWtobAfryKDR8HvkssZdzNkbUj5PT1DR3vwJrzv9JsicErwTSYSD3mIgNO
 
