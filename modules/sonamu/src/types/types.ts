9655a4e2a9e1927008ee4a32784f6682769b050d
DongSeonYoo <inko51366@naver.com>
2025-12-19 00:22:36 +0900

    [sonamu]: semanticSearch를 위한 SonamuSemanticParams 타입 정의

--- modules/sonamu/src/types/types.ts
+++ modules/sonamu/src/types/types.ts
@@ -492,6 +492,19 @@ export type SubsetQuery = {
 export const SonamuQueryMode = z.enum(["both", "list", "count"]);
 export type SonamuQueryMode = z.infer<typeof SonamuQueryMode>;
 
+/* Semantic Query */
+export const SonamuSemanticParams = z
+  .object({
+    semanticQuery: z.object({
+      embedding: z.array(z.number()).min(1024).max(1024),
+      threshold: z.number().optional(),
+      method: z.enum(["cosine", "l2", "inner_product"]).optional(),
+      which: z.string(),
+    }),
+  })
+  .partial();
+export type SonamuSemanticParams = z.infer<typeof SonamuSemanticParams>;
+
 /* Knex Migration */
 export type KnexError = {
   code: string;
