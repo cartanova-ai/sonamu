a785459063280a557fdd92971b857b9ba4a5f557
nakkim <nakkim.dev@gmail.com>
2025-12-24 15:30:37 +0900

    [create-sonamu] SON-182 sonamu test 통합 반영

--- modules/create-sonamu/template/src/api/src/testing/fixture.ts
+++ modules/create-sonamu/template/src/api/src/testing/fixture.ts
@@ -1,18 +1,6 @@
-const fixtureLoader = {
+import { createFixtureLoader } from "sonamu/test";
+
+export const loadFixtures = createFixtureLoader({
   // Define your fixtures...
   fixture01: async () => {},
-};
-
-export async function loadFixtures<K extends keyof typeof fixtureLoader>(
-  names: K[],
-): Promise<{
-  [P in K]: Awaited<ReturnType<(typeof fixtureLoader)[P]>>;
-}> {
-  return Object.fromEntries(
-    await Promise.all(
-      names.map(async (name) => {
-        return [name, await fixtureLoader[name]()];
-      }),
-    ),
-  );
-}
+});
