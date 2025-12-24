8b085e87e175d1e1db1e9df181f7243ebe73d94b
nakkim <nakkim.dev@gmail.com>
2025-12-24 15:39:04 +0900

    [sonamu] SON-182 FixtureLoader 설정 헬퍼 추가

---
+++ modules/sonamu/src/testing/fixture-loader.ts
@@ -0,0 +1,28 @@
+/**
+ * Fixture Loader Factory
+ *
+ * 테스트에서 사용할 fixture를 로드하는 함수를 생성
+ *
+ * @example
+ * ```typescript
+ * // fixture.ts
+ * import { createFixtureLoader } from "sonamu/test";
+ *
+ * export const loadFixtures = createFixtureLoader({
+ *   company01: async () => CompanyModel.findById("A", 1),
+ *   user01: async () => UserModel.findById("A", 1),
+ * });
+ *
+ * // test.ts
+ * const { company01, user01 } = await loadFixtures(["company01", "user01"]);
+ * ```
+ */
+export function createFixtureLoader<T extends Record<string, () => Promise<unknown>>>(loaders: T) {
+  return async function loadFixtures<K extends keyof T>(
+    names: K[],
+  ): Promise<{ [P in K]: Awaited<ReturnType<T[P]>> }> {
+    return Object.fromEntries(
+      await Promise.all(names.map(async (name) => [name, await loaders[name]()])),
+    );
+  };
+}
--- modules/sonamu/src/testing/index.ts
+++ modules/sonamu/src/testing/index.ts
@@ -1,2 +1,3 @@
 export * from "./bootstrap";
+export * from "./fixture-loader";
 export * from "./naite-vitest-reporter";
