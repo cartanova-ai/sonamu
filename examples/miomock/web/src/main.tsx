d549e971c9d82384a7bf75b253d9faed011edd36
seoyoungsz <choisy9619@gmail.com>
2025-12-26 11:53:35 +0900

    [miomock/web] semantic-ui 의존성 제거

--- examples/miomock/web/src/main.tsx
+++ examples/miomock/web/src/main.tsx
@@ -3,7 +3,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "semantic-ui-css/semantic.min.css";
 import "./styles/tailwind.css";
 import App from "./App";
 import { AuthProvider } from "./admin-common/auth";
