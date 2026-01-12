8012c0f20e7ad8cefa8653f406556f78f30f99c9
seoyoungsz <choisy9619@gmail.com>
2026-01-12 19:39:52 +0900

    [sonamu/puri] chore: test 코드에서 .only 수정자 제거

--- examples/miomock/api/src/sonamu-test/puri-query.test.ts
+++ examples/miomock/api/src/sonamu-test/puri-query.test.ts
@@ -259,7 +259,7 @@ describe("Puri Query", () => {
         );
       });
 
-      test.only("where로 NULL 체크", async () => {
+      test("where로 NULL 체크", async () => {
         const db = UserModel.getPuri("r");
         await db.table("employees").where("employees.hire_date", null);
         const query = Naite.get("puri:executed-query").first();
@@ -267,7 +267,7 @@ describe("Puri Query", () => {
         expectQuery(query, "where").toMatchInlineSnapshot(`""employees"."hire_date" IS NULL"`);
       });
 
-      test.only("where로 NOT NULL 체크", async () => {
+      test("where로 NOT NULL 체크", async () => {
         const db = UserModel.getPuri("r");
         await db.table("employees").where("employees.hire_date", "!=", null);
         const query = Naite.get("puri:executed-query").first();
