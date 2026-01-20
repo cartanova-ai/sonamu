c207ea2792a0c8cfd84b25771d04dba6892897fa
noasouth <noa@cartanova.ai>
2026-01-20 14:40:21 +0900

    [sonamu] SON-264 global.ts에서 sonamu/test의 setup 함수를 사용하도록 변경

--- modules/create-sonamu/template/src/packages/api/src/testing/global.ts
+++ modules/create-sonamu/template/src/packages/api/src/testing/global.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-export async function setup() {
-  return async function teardown() {};
-}
+
+// sonamu.config.ts의 test 설정을 기반으로 병렬 테스트 환경을 구성합니다.
+export { setup } from "sonamu/test";
