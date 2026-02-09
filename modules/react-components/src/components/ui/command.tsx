9d759d0a627a09c7718d99a953a595a212f610ea
nakkim <nakkim.dev@gmail.com>
2026-02-09 15:27:57 +0900

    [react-components] SON-205 CommandDialog에 commandProps 전달 지원 추가

--- modules/react-components/src/components/ui/command.tsx
+++ modules/react-components/src/components/ui/command.tsx
@@ -23,11 +23,22 @@ const Command = React.forwardRef<
 ));
 Command.displayName = CommandPrimitive.displayName;
 
-const CommandDialog = ({ children, ...props }: DialogProps) => {
+type CommandDialogProps = DialogProps & {
+  commandProps?: React.ComponentPropsWithoutRef<typeof CommandPrimitive>;
+};
+
+const CommandDialog = ({ children, commandProps, ...props }: CommandDialogProps) => {
+  const { className: commandClassName, ...restCommandProps } = commandProps ?? {};
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0 shadow-lg">
-        <Command className="**:[[cmdk-group-heading]]:px-2 **:[[cmdk-group-heading]]:font-medium **:[[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 **:[[cmdk-group]]:px-2 **:[[cmdk-input-wrapper]_svg]:size-5 **:[[cmdk-input]]:h-12 **:[[cmdk-item]]:px-2 **:[[cmdk-item]]:py-3 **:[[cmdk-item]_svg]:size-5">
+        <Command
+          className={cn(
+            "**:[[cmdk-group-heading]]:px-2 **:[[cmdk-group-heading]]:font-medium **:[[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 **:[[cmdk-group]]:px-2 **:[[cmdk-input-wrapper]_svg]:size-5 **:[[cmdk-input]]:h-12 **:[[cmdk-item]]:px-2 **:[[cmdk-item]]:py-3 **:[[cmdk-item]_svg]:size-5",
+            commandClassName,
+          )}
+          {...restCommandProps}
+        >
           {children}
         </Command>
       </DialogContent>
